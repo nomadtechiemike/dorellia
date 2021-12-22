0f4d1abb9d40757753c49eae7e8c2ead095aa0b3
Thor <sg1asgard@outlook.com>
2021-12-22 15:39:06 +0200

    adding boostrap icon

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '~/assets/scss/main.scss',
-    '~/node_modules/bootstrap-icons/font/bootstrap-icons'
+    '~/node_modules/bootstrap-icons/font/bootstrap-icons.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
