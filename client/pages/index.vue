87df2b1c31ba03dcbd528d7d3c9e721241cb6833
Thor <sg1asgard@outlook.com>
2021-12-23 17:47:46 +0200

    getting our first data form api

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -2,15 +2,24 @@
   <div class="row">
     <div class="col-12">
       <PageTitle page_title="Starters" />
-      <p>
-        Dolore qui fugit et quae quia velit et quae. Est exercitationem iste
-        magni. Aliquid commodi ad assumenda rerum debitis sed non. Dolorum neque
-        doloremque nostrum. Atque amet fugiat quia veniam consectetur. Ratione
-        sapiente perferendis qui incidunt tempore voluptatem molestias.
-      </p>
-      <p>
-        <button type="button" class="btn btn-primary">Primary</button>
-      </p>
+
+      <div>
+        <div
+          v-for="(item, index) in menu.data"
+          :key="index"
+          class="pb-3">
+          <div v-for="(image, index) in item.attributes.Image" :key="index">
+            <img :src="`http://localhost:1337${image.attributes.formats.medium.url}`" alt="">
+          </div>
+          <!-- <div><img :src="item.Image.data.attributes.formats.medium.url" alt=""></div> -->
+          <div :style="`background-color: #${item.attributes.Color}`">{{ item.attributes.Color}}</div>
+          <div>{{ item.attributes.title }}</div>
+          <div>{{ item.attributes.Description}}</div>
+          <div>{{ item.attributes.Price}}</div>
+          <button class="btn btn-primary">add to cart</button>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -19,5 +28,27 @@
 export default {
   layout: 'homepage',
   name: 'starters',
+  data() {
+    return {
+      menu: [],
+    }
+  },
+
+  created() {
+    this.getMenu()
+  },
+
+  methods: {
+    async getMenu() {
+      try {
+        const results = await this.$strapi.find('menu-combos', {
+          populate: ['*']
+        })
+        this.menu = results
+      } catch (error) {
+        console.log('something went wrong: ', error)
+      }
+    },
+  },
 }
 </script>
