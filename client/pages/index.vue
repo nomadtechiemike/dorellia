7336f0121ad46201ecfa78c795c6908d113c2799
Thor <sg1asgard@outlook.com>
2021-12-26 15:14:43 +0200

    switching to qs strapi and axios

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -2,25 +2,7 @@
   <div class="row">
     <div class="col-12">
       <PageTitle page_title="Starters" />
-
-      <div>
-        <div v-for="(item, index) in menu.data" :key="index" class="pb-3">
-          <div v-for="(image, index) in item.attributes.Image" :key="index">
-            <img
-              :src="`http://localhost:1337${image.attributes.formats.medium.url}`"
-              alt=""
-            />
-          </div>
-          <!-- <div><img :src="item.Image.data.attributes.formats.medium.url" alt=""></div> -->
-          <div :style="`background-color: #${item.attributes.Color}`">
-            {{ item.attributes.Color }}
-          </div>
-          <div>{{ item.attributes.title }}</div>
-          <div>{{ item.attributes.Description }}</div>
-          <div>{{ item.attributes.Price }}</div>
-          <button class="btn btn-primary">add to cart</button>
-        </div>
-      </div>
+      <MenuCombos :menu_combos="menu" />
     </div>
   </div>
 </template>
@@ -46,8 +28,8 @@ export default {
         const query = qs.stringify(
           {
             filters: {
-              title: {
-                $eq: 'Junior Starter',
+              combotype: {
+                $eq: 'starter',
               },
             },
           },
@@ -59,9 +41,6 @@ export default {
         const results = await this.$axios.$get(
           `http://localhost:1337/api/menu-combos/?populate=*&${query}`
         )
-        // const results = await this.$strapi.find('menu-combos', {
-        //   populate: ['*'],
-        // })
         this.menu = results
 
       } catch (error) {
