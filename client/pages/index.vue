11f45f45b3c11fac39f874f3b5401e94cae3e50f
Thor <sg1asgard@outlook.com>
2021-12-26 14:18:00 +0200

    switching to qs strapi and axios

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -4,22 +4,23 @@
       <PageTitle page_title="Starters" />
 
       <div>
-        <div
-          v-for="(item, index) in menu.data"
-          :key="index"
-          class="pb-3">
+        <div v-for="(item, index) in menu.data" :key="index" class="pb-3">
           <div v-for="(image, index) in item.attributes.Image" :key="index">
-            <img :src="`http://localhost:1337${image.attributes.formats.medium.url}`" alt="">
+            <img
+              :src="`http://localhost:1337${image.attributes.formats.medium.url}`"
+              alt=""
+            />
           </div>
           <!-- <div><img :src="item.Image.data.attributes.formats.medium.url" alt=""></div> -->
-          <div :style="`background-color: #${item.attributes.Color}`">{{ item.attributes.Color}}</div>
+          <div :style="`background-color: #${item.attributes.Color}`">
+            {{ item.attributes.Color }}
+          </div>
           <div>{{ item.attributes.title }}</div>
-          <div>{{ item.attributes.Description}}</div>
-          <div>{{ item.attributes.Price}}</div>
+          <div>{{ item.attributes.Description }}</div>
+          <div>{{ item.attributes.Price }}</div>
           <button class="btn btn-primary">add to cart</button>
         </div>
       </div>
-
     </div>
   </div>
 </template>
@@ -41,10 +42,28 @@ export default {
   methods: {
     async getMenu() {
       try {
-        const results = await this.$strapi.find('menu-combos', {
-          populate: ['*']
-        })
+        const qs = require('qs')
+        const query = qs.stringify(
+          {
+            filters: {
+              title: {
+                $eq: 'Junior Starter',
+              },
+            },
+          },
+          {
+            encodeValuesOnly: true,
+          }
+        )
+
+        const results = await this.$axios.$get(
+          `http://localhost:1337/api/menu-combos/?populate=*&${query}`
+        )
+        // const results = await this.$strapi.find('menu-combos', {
+        //   populate: ['*'],
+        // })
         this.menu = results
+
       } catch (error) {
         console.log('something went wrong: ', error)
       }
