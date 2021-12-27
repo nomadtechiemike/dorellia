dbdc17eb37841d6ce9fcfa66bd71234550fe6ba5
Thor <sg1asgard@outlook.com>
2021-12-27 13:03:15 +0200

    styling menu combos component

---
+++ client/components/AddToCart.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="d-inline-block">
+    <button class="btn btn-primary mt-3 add-to-cart">add to cart</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.btn-primary {
+  &.add-to-cart {
+    background-color: lighten($color: $primary, $amount: 30);
+  }
+}
+</style>
--- client/components/MenuCombos.vue
+++ client/components/MenuCombos.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="menu-combos d-flex flex-column flex-md-row">
+  <div class="menu-combos d-flex flex-column flex-lg-row mt-4">
     <div v-for="(item, index) in menu_combos.data" :key="index">
       <div
         v-for="(image, index) in item.attributes.Image"
         :key="index"
         :style="`background-image: url(http://localhost:1337${image.attributes.formats.medium.url})`"
-        class="menu-combos__image"
+        class="menu-combos__image d-flex align-items-center"
       >
-        <div>
+        <div class="w-100">
           <div
             :style="`background-color: #${item.attributes.Color}`"
             class="text-center"
@@ -18,10 +18,11 @@
       </div>
 
       <div class="d-flex mt-4">
-        <div class="lex-grow-1">{{ item.attributes.Description }}</div>
+        <div class="flex-fill">{{ item.attributes.Description }}</div>
         <div class="price">${{ item.attributes.Price }}</div>
       </div>
-      <button class="btn btn-primary mt-3">add to cart</button>
+
+      <AddToCart />
     </div>
   </div>
 </template>
@@ -38,24 +39,37 @@ export default {
   h2 {
     font-size: 2.3333333333333335rem;
   }
-  > div {
-    border-right: 1px dotted $primary;
-    margin-right: 20px;
-    padding-right: 20px;
-    &:last-child {
+  @media screen and (max-width: 992px) {
+    > div {
+      border-right: none;
+      border-bottom: 1px dotted $primary;
       margin-right: 0;
       padding-right: 0;
-      border: none;
+      margin-bottom: 20px;
+      padding-bottom: 20px;
+    }
+  }
+  @media screen and (min-width: 992px) {
+    > div {
+      border-right: 1px dotted $primary;
+      margin-right: 20px;
+      padding-right: 20px;
+      &:last-child {
+        margin-right: 0;
+        padding-right: 0;
+        border: none;
+      }
     }
   }
   &__image {
-    padding: 10px;
+    padding: 20px;
+    height: 244px;
     > div {
       padding: 10px;
       margin: 10px;
       border: 1px solid white;
       > div {
-        padding: 10px;
+        padding: 20px;
         margin: 10px;
         color: white;
       }
@@ -64,6 +78,7 @@ export default {
   .price {
     font-size: 1.6666666666666667rem;
     font-weight: 500;
+    padding-left: 15px;
   }
 }
 </style>
