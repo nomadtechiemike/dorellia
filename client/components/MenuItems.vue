c778cc8c9ddbaa92fc7f1d3fd43c05611e786e82
Thor <sg1asgard@outlook.com>
2021-12-27 15:14:16 +0200

    adding content to the other pages

---
+++ client/components/MenuItems.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="menu-items d-flex flex-column flex-lg-row mt-4">
+    <div v-for="(item, index) in menu_items.data" :key="index">
+      <div
+        v-for="(image, index) in item.attributes.image"
+        :key="index"
+      >
+        <img :src="`http://localhost:1337${image.attributes.formats.thumbnail.url}`" :alt="image.attributes.alternativeText" />
+      </div>
+      <div>{{ item.attributes.title }}</div>
+      <div>{{ item.attributes.description }}</div>
+      <div>{{ item.attributes.price }}</div>
+      <AddToCart />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['menu_items'],
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss">
+.menu-items {
+}
+</style>
--- client/pages/lunch.vue
+++ client/pages/lunch.vue
@@ -2,7 +2,8 @@
   <div class="row">
     <div class="col-12">
       <PageTitle page_title="Lunch" />
-      <MenuCombos :menu_combos="menu" />
+      <MenuCombos :menu_combos="menuCombos" />
+      <MenuItems :menu_items="menuItems" />
     </div>
   </div>
 </template>
@@ -13,16 +14,18 @@ export default {
   name: 'lunch',
   data() {
     return {
-      menu: [],
+      menuCombos: [],
+      menuItems: [],
     }
   },
 
   created() {
-    this.getMenu()
+    this.getMenuCombos()
+    this.getMenuItems()
   },
 
   methods: {
-    async getMenu() {
+    async getMenuCombos() {
       try {
         const qs = require('qs')
         const query = qs.stringify(
@@ -41,7 +44,33 @@ export default {
         const results = await this.$axios.$get(
           `http://localhost:1337/api/menu-combos/?populate=*&${query}`
         )
-        this.menu = results
+        this.menuCombos = results
+
+      } catch (error) {
+        console.log('something went wrong: ', error)
+      }
+    },
+
+    async getMenuItems() {
+      try {
+        const qs = require('qs')
+        const query = qs.stringify(
+          {
+            filters: {
+              itemtype: {
+                $eq: 'lunch',
+              },
+            },
+          },
+          {
+            encodeValuesOnly: true,
+          }
+        )
+
+        const results = await this.$axios.$get(
+          `http://localhost:1337/api/menu-items/?populate=*&${query}`
+        )
+        this.menuItems = results
 
       } catch (error) {
         console.log('something went wrong: ', error)
