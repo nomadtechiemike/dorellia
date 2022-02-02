891d71de2c0f9c745ac9dc6131c1f45e99ac708a
Thor <sg1asgard@outlook.com>
2022-02-02 12:39:22 +0200

    setting up the store

---
+++ client/store/oder.js
@@ -0,0 +1,15 @@
+export const state = () => ({
+// State
+})
+
+export const getters = () => ({
+// Getters
+})
+
+export const mutations = {
+// Mutations
+}
+
+export const actions = () => {
+// Actions
+}
